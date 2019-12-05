fb4e1abb2d15a00ac392537cd2787da2d95c1e70
Kus Cámara <kcmr@users.noreply.github.com>
2019-12-05 02:28:00 +0100

    basic commands and options with yargs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ const yargs = require('yargs');
 
 module.exports = {
 	run() {
-		console.log(yargs.argv);
+		// Los comandos y descripcion de cada uno aparecen en la ayuda (di-hola --help)
+		// comando --version nos muestra la version del package.json
+		// yargs API: https://github.com/yargs/yargs/blob/HEAD/docs/api.md
+		yargs
+			.command({
+				command: 'saluda',
+				describe: 'Dice hola', 
+				handler() {
+					console.log(`Hola ${yargs.argv.nombre}`);
+				}
+			})
+			.command({
+				command: 'insulta',
+				describe: 'Insulta',
+				handler() {
+					console.log('Te digo cosas feas');
+				}
+			})
+			.option('nombre', {
+				alias: 'n',
+				default: 'persona'
+			})
+			.argv;
 	}
 };
